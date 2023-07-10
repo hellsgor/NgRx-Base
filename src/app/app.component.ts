f0a93610b5900ab4391fd60d7a8d69cf33b077d6
hellsgor <hellsgor@gmail.com>
2023-07-10 15:26:16 +0400

    add counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'ngrx-base-grom';
+  public counter = 0;
+  public updatedAt?: number;
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  public increase(): void {
+    this.updatedAt = Date.now();
+    this.counter++;
+  }
+
+  public decrease(): void {
+    this.updatedAt = Date.now();
+    this.counter--;
+  }
+
+  public clear(): void {
+    this.updatedAt = Date.now();
+    this.counter = 0;
+  }
 }
